e897f04c3e2cae0670100ef768882ece1457d215
Pei-Yi Lin <plin@stytch.com>
2024-10-22 10:22:05 -0700

    Change back to log

--- main.go
+++ main.go
@@ -162,7 +162,7 @@ func (s *AuthService) authenticateHandler(w http.ResponseWriter, r *http.Request
 			DiscoveryMagicLinksToken: token,
 		})
 		if err != nil {
-			fmt.Printf("Error authenticating: %v\n", err)
+			log.Printf("Error authenticating: %v\n", err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
